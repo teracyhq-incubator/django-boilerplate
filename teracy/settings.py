89592d7f19488ba18ca01110b6b6f27e1831a881
Hoat Le <hoatlevan@gmail.com>
2013-05-27 17:57:39 +0700

    static file serving on heroku

--- teracy/settings.py
+++ teracy/settings.py
@@ -1,6 +1,6 @@
 # Django settings for teracy project.
 import os
-from os import  environ
+from os import environ
 
 PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
