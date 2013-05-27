8ab6ecc272efa38f0af6e84c5313fc132a7e4e0c
Hoat Le <hoatlevan@gmail.com>
2013-05-27 18:47:18 +0700

    Delete default teracy/settings.py; use settings default module, add heroku setting on prod.py settings

--- settings/default.py
+++ settings/default.py
@@ -165,12 +165,3 @@ LOGGING = {
             },
         }
 }
-
-#TODO move this to prod?
-# Parse database configuration from $DATABASE_URL
-if environ.has_key('DATABASE_URL'):
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- settings/prod.py
+++ settings/prod.py
@@ -1,2 +1,11 @@
 # settings for production
 from default import *
+
+#heroku
+# Parse database configuration from $DATABASE_URL
+if environ.has_key('DATABASE_URL'):
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
