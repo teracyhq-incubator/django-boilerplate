d0828d4ccf896f2618a7f557645c85f278fe7edf
Hoat Le <hoatlevan@gmail.com>
2013-06-12 04:26:36 +0700

    Add django-compressor, django-debug-toolbar

--- settings/dev.py
+++ settings/dev.py
@@ -1,6 +1,20 @@
 # settings for development
 from common import *
 
+INTERNAL_IPS = ('127.0.0.1',)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+
+STATICFILES_FINDERS += (
+    'compressor.finders.CompressorFinder',
+)
+
 INSTALLED_APPS += (
     'django.contrib.admin',
+    'debug_toolbar',
+    'compressor',
+    'teracy.html5boilerplate',
 )
