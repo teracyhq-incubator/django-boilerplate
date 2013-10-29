7c2f2afa80aee19f34862dea1bfc6a4e29c88347
Hoat Le <hoatlevan@gmail.com>
2013-10-29 13:59:21 +0000

    DJBP-8 | proper prod settings following heroku

--- settings/prod.py
+++ settings/prod.py
@@ -23,3 +23,16 @@ if 'DATABASE_URL' in os.environ:
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
